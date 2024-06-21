1fc91558d62ddb95acadb0701151bf2f98ba38aa
Štěpán Postler <stepan.postler@gmail.com>
2024-06-21 10:24:28 +0200

    wip

--- bin/install.php
+++ bin/install.php
@@ -5,12 +5,12 @@ namespace Bin;
 require __DIR__ . '/../vendor/autoload.php';
 
 use App\Bootstrap;
-use Webovac\Core\Command\MigrateAndInstallCommand;
+use Webovac\Core\Command\InstallCommand;
+use Webovac\Core\Command\MigrateCommand;
 
 
 exec(__DIR__ . '/clear-cache');
-Bootstrap::boot()
-	->createContainer()
-	->getByType(MigrateAndInstallCommand::class)
-	->run();
+Bootstrap::boot()->createContainer()->getByType(MigrateCommand::class)->run();
+exec(__DIR__ . '/clear-cache');
+Bootstrap::boot()->createContainer()->getByType(InstallCommand::class)->run();
 exec(__DIR__ . '/clear-cache');
